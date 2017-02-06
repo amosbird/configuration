8d7206929a01ee3c4006a2e55d6796936cc38844
Amos Bird <amosbird@gmail.com>
2017-02-06 11:44:35 +0800

    add vivaldi profile.

--- backup.sh
+++ backup.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-shopt -s extglob
-cp -r ~/.weechat/!(logs) weechat/
-tar czf weechat.tar.gz weechat
+rsync -a --exclude='logs' ~/.weechat .
+tar czf weechat.tar.gz .weechat
 rm weechat.tar.gz.gpg
 gpg --recipient amosbird@gmail.com --encrypt weechat.tar.gz
 rm weechat.tar.gz
+
+rsync -a --exclude='Cache' --exclude='Media Cache' --exclude='Local Storage' ~/.config/vivaldi .
+tar czf vivaldi.tar.gz vivaldi
+rm vivaldi.tar.gz.gpg
+gpg --recipient amosbird@gmail.com --encrypt vivaldi.tar.gz
+rm vivaldi.tar.gz
+rm -rf vivaldi
+rm -rf .weechat
